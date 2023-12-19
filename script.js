b59b1ba7b34569152b9a202526eda36828a89a6c
Flore <rantswillerflore@gmail.com>
2023-12-19 09:56:21 +0100

    base

---
+++ script.js
@@ -0,0 +1,6 @@
+const commentList = document.getElementById ("comment-List");
+const errorMessage = document.getElementById ("error-message");
+const form = document.getElementsByTagName ("form");
+const firstName = document.getElementById ("first-name");
+const lastName = document.getElementById ("last-name");
+const message = document.getElementById ("message");
